2c541232aaba7330b3f9c16ca0a900ee19912042
yuyulusu <148343109+yuyulusu@users.noreply.github.com>
2024-05-25 23:41:32 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
 API_URL = 'https://1c3a36d9-dac1-4583-bd5e-d8e85b9e19d1.serverhub.praktikum-services.ru'
 ORDER_ENDPOINT = '/api/v1/orders'
-TRACK_ENDPOINT - '/api/v1/orders/track'
+TRACK_ENDPOINT = '/api/v1/orders/track'
