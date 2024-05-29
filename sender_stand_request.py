654b9dd34ea0685800b9152fa12a93d1da900c91
yuyulusu <148343109+yuyulusu@users.noreply.github.com>
2024-05-29 17:08:08 +0300

    Файл для запуска теста через консоль

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,14 +1,31 @@
 import requests
 import unittest
 import pytest
-from configuration import API_URl, ORDER_ENDPOINT, TRACK_ENDPOINT
-from data import order_data
-
+#from configuration import API_URl, ORDER_ENDPOINT, TRACK_ENDPOINT
+#from data import order_data
+API_URL = 'https://f9b63350-120a-4f97-ae40-babd83ae54bf.serverhub.praktikum-services.ru'
+ORDER_ENDPOINT = '/api/v1/orders'
+TRACK_ENDPOINT = '/api/v1/orders/track'
+order_data = {
+  "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2020-06-06",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
 class TestOrderGet(unittest.TestCase):
+        API_URL = 'https://f9b63350-120a-4f97-ae40-babd83ae54bf.serverhub.praktikum-services.ru'
 
         def test_create_and_get_order(self):
   
                 # Выполнить запрос на создание заказа
+                print(API_URL+ORDER_ENDPOINT)
                 create_response = requests.post(API_URL+ORDER_ENDPOINT, json=order_data)
                 self.assertEqual(create_response.status_code, 201, "Order creation failed")
                 
@@ -30,3 +47,4 @@ class TestOrderGet(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+    
